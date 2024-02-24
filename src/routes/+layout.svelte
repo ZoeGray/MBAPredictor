ca3838a7f0366a8ce7cab3181c93779e8108825c
Zoe Gray <zleighgray@gmail.com>
2024-02-24 17:12:41 -0600

    small details

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,18 @@
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 
     $: activeUrl = $page.url.pathname;
-    let activeClass = 'font-bold text-offwhite hover:text-orange';
-    let nonActiveClass = 'text-offwhite hover:text-orange';
+    let activeClass = 'underline underline-offset-8';
 </script>
 
-<Navbar class="border rounded-lg outline outline-double outline-8 outline-black bg-darkblue py-8">
+<Navbar class="bg-darkblue py-8">
     <NavBrand>
         <Icon icon="emojione:basketball" class="size-32 self-center whitespace-nowrap text-xl font-semibold dark:text-white"/>
-        <span class="self-center whitespace-nowrap font-semibold text-orange font-serif text-6xl ps-8">NBA Game Predictions</span>
+        <span class="self-center whitespace-nowrap font-semibold text-orange font-serif text-6xl px-8">NBA Game Predictions</span>
     </NavBrand>
-    <NavUl {activeUrl} {activeClass} {nonActiveClass}>
-        <NavLi href="/" class='size-lg'>Home</NavLi>
-        <NavLi href="/about" class='size-lg'>About</NavLi>
-        <NavLi href="/data" class='size-lg'>Data</NavLi>
+    <NavUl {activeUrl} {activeClass}>
+        <NavLi href="/" class='text-offwhite hover:text-orange'>Home</NavLi>
+        <NavLi href="/about" class='text-offwhite hover:text-orange'>About</NavLi>
+        <NavLi href="/data" class='text-offwhite hover:text-orange'>Data</NavLi>
     </NavUl>
 </Navbar>
 
