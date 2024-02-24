6db8bcc577eb257fea945dd060172ded51f0993f
Zoe Gray <zleighgray@gmail.com>
2024-02-24 14:55:58 -0600

    Creating our first component, getting everything working

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import 'flowbite-svelte';
+    import Icon from '@iconify/svelte';
+    import "../app.pcss";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar class="fluid">
+    <NavBrand>
+        <Icon icon="emojione:basketball" class="size-32 self-center whitespace-nowrap text-xl font-semibold dark:text-white"/>
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NBA Predictor</span>
+    </NavBrand>
+    <NavHamburger  />
+    <NavUl >
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/about">About</NavLi>
+        <NavLi href="/docs/components/navbar">Data</NavLi>
+    </NavUl>
+</Navbar>
+
+<slot />
